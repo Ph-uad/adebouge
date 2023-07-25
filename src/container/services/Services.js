54029208515443a6da7390fb352d7b3be7abb3fa
Ph-uad <temidayo26j@gmail.com>
2023-07-25 20:57:33 +0100

    added a modal feature

--- src/container/services/Services.js
+++ src/container/services/Services.js
@@ -1,28 +1,54 @@
+import { useState } from 'react'
 import classes from './services.module.scss'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const Services = ({ data }) => {
 
+  const [selectedId, setSelectedId] = useState(null)
+
   const list = data?.map((item, i) => (
-    <li key={ i } className={`item ${classes.list }`}>
+    <motion.li key={ i } className={`item ${classes.list }`} layoutId={item.id} onClick={() => setSelectedId(item)}>
       <figure className={ classes.list__figure }>
         <img src={ item.poster } alt={ item.title } />
       </figure>
       <article>
         <h2 className='heading-secondary'>{ item.title }</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, modi voluptatibus cumque incidunt magni suscipit.</p>
+        <p className='ellipse'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, modi voluptatibus cumque incidunt magni suscipit.</p>
       </article>
-      <a href="" className="link">Learn More</a>
-    </li>
+      <button href="" className="link">Learn More</button>
+    </motion.li>
   ))
 
   return (
-    <section className="">
+    <div className='position-relative'>
+      <section>
       <div className="container">
         <ul className='flex'>
           { list }
         </ul>
       </div>
-    </section>
+      </section>
+
+      <AnimatePresence>
+   {selectedId && (
+     <motion.div 
+      layoutId={selectedId}
+      className={classes.modal}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      >
+       <motion.button onClick={() => setSelectedId(null)}>&#10060;</motion.button>
+       <figure className={ classes.list__figure }>
+        <img src={ selectedId.poster } alt={ selectedId.title } />
+      </figure>
+       <motion.h2>{selectedId.title}</motion.h2>
+       <motion.h5>item.subtitle</motion.h5>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat qui ratione facere itaque temporibus voluptates, sit minima. Necessitatibus, explicabo? Autem.</p>
+     </motion.div>
+   )}
+      </AnimatePresence>
+    </div>
   )
 }
 export default Services;
