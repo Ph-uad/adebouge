beb33574ce003e178479ff4b34b71214f267b328
Ph-uad <temidayo26j@gmail.com>
2023-07-27 15:09:12 +0100

    centerred header text

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
         <header className={ classes.header }>
-            <div className="container">
+            <div className="container text-center">
                 <h1>Adebouge International</h1>
                 <h3>Creating innovavative opportunity window for emerging communities</h3>
             </div>
