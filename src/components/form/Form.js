81599c30b5e9d5764f79763938b4cf47d85dee3a
Ph-uad <temidayo26j@gmail.com>
2023-07-30 12:43:01 +0100

    Form input and label responsive and style added

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,34 +1,30 @@
 import classes from './form.module.scss'
 
 const Form = () => {
-  return (
-    <form action="" className="">
-        <div className={`container ${classes.form__container}`}>
-            <div className="form-control half">
-                <input autoComplete type="text" name='name' placeholder='First and Last Name' id='name'/>
-                <label htmlFor="name">Name</label>
-            </div>
-            <div className="form-control half">
-                <input autoComplete type="text" placeholder='Company Name' name='comapny' id='comapny'/>
-                <label htmlFor="comapny">Comapny Name</label>
-            </div>
-            <div className="form-control">
-                <input autoComplete type="email" placeholder='johndoe@gmail.com' name='email' id='email'/>
-                <label htmlFor="email">Email</label>
-            </div>
-            <div className="form-control">
-             <select name="" id="">
-                <option value="Social ">Social</option>
-                <option value="Data ">Data</option>
-             </select>
-            </div>
-            <div className="form-control">
-                <textarea name="message" placeholder='Send us a message' id="message" cols="30" rows="10"></textarea>
-                <label htmlFor="message">Message</label>
-            </div>
-        </div>
-    </form>
-  )
+    return (
+        <section className={classes.form}>
+            <form action="" className="container">
+                <div className={ `${classes.form__container}` }>
+                    <div className={ classes.form__control }>
+                        <input autoComplete='true' type="text" name='name' placeholder='First and Last Name' id='name' />
+                        <label htmlFor="name">Names</label>
+                    </div>
+                    <div className={ classes.form__control }>
+                        <input autoComplete='true' type="text" placeholder='Company Name' name='comapny' id='comapny' />
+                        <label htmlFor="comapny">Comapny's Name</label>
+                    </div>
+                    <div className={ classes.form__control }>
+                        <input autoComplete='true' type="email" placeholder='johndoe@gmail.com' name='email' id='email' />
+                        <label htmlFor="email">Email</label>
+                    </div>
+                    <div className={ classes.form__control }>
+                        <textarea name="message" placeholder='Send us a message' id="message" cols="30" rows="10"></textarea>
+                        <label htmlFor="message">Message</label>
+                    </div>
+                </div>
+            </form>
+        </section>
+    )
 }
 
 export default Form
