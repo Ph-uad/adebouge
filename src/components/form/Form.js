6d40ab1f026fc558d20a2ba6ddffc1690373c888
Ph-uad <temidayo26j@gmail.com>
2023-08-04 14:15:29 +0100

    form styling (background)

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,8 +2,8 @@ import classes from './form.module.scss'
 
 const Form = () => {
     return (
-        <section className={classes.form}>
-            <form action="" className="container">
+        <section className={ classes.form }>
+            <form action="" className=" ">
                 <div className={ `${classes.form__container}` }>
                     <div className={ classes.form__control }>
                         <input autoComplete='true' type="text" name='name' placeholder='First and Last Name' id='name' />
@@ -21,6 +21,17 @@ const Form = () => {
                         <textarea name="message" placeholder='Send us a message' id="message" cols="30" rows="10"></textarea>
                         <label htmlFor="message">Message</label>
                     </div>
+
+                    {/* <div className={ classes.form__control }>
+                        <input type="radio" name="join" id="join" />
+                        <label htmlFor="join">Sign Up</label>
+                    </div>
+                    <div className={ classes.form__control }>
+                        <input type="radio" name="join" id="joins" />
+                        <label htmlFor="joins">Email</label>
+                    </div> */}
+
+                    <button type="submit" className={`button ${classes.button}`}>Join Innitiative</button>
                 </div>
             </form>
         </section>
