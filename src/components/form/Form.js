b72e476dfbfc4262f6eb6aa82c270a16d340e2fc
Ph-uad <temidayo26j@gmail.com>
2023-08-05 18:50:46 +0100

    form design

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -13,25 +13,28 @@ const Form = () => {
                         <input autoComplete='true' type="text" placeholder='Company Name' name='comapny' id='comapny' />
                         <label htmlFor="comapny">Comapny's Name</label>
                     </div>
-                    <div className={ classes.form__control }>
+                    {/* <div className={ classes.form__control }>
                         <input autoComplete='true' type="email" placeholder='johndoe@gmail.com' name='email' id='email' />
                         <label htmlFor="email">Email</label>
-                    </div>
+                    </div> */}
                     <div className={ classes.form__control }>
                         <textarea name="message" placeholder='Send us a message' id="message" cols="30" rows="10"></textarea>
                         <label htmlFor="message">Message</label>
                     </div>
-
-                    {/* <div className={ classes.form__control }>
-                        <input type="radio" name="join" id="join" />
-                        <label htmlFor="join">Sign Up</label>
+                    
+                    <div className="flex">
+                        <div className={ classes.form__control }>
+                            <input type="radio" name="join" id="join" />
+                            <label htmlFor="join">Sign Up</label>
+                        </div>
+                        <div className={ classes.form__control }>
+                            <input type="radio" name="join" id="joins" />
+                            <label htmlFor="joins">Email</label>
+                        </div>
                     </div>
-                    <div className={ classes.form__control }>
-                        <input type="radio" name="join" id="joins" />
-                        <label htmlFor="joins">Email</label>
-                    </div> */}
 
-                    <button type="submit" className={`button ${classes.button}`}>Join Innitiative</button>
+
+                    <button type="submit" className={ `button ${classes.button}` }>Join Innitiative</button>
                 </div>
             </form>
         </section>
